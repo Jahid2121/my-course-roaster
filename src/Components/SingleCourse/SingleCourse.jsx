c7e5a6b8cf303ef39438be9bbff04d7dfeec7663
Jahid2121 <jahidsarkar2121@gmail.com>
2023-09-16 22:36:52 +0600

    Import and add react icon

--- src/Components/SingleCourse/SingleCourse.jsx
+++ src/Components/SingleCourse/SingleCourse.jsx
@@ -1,4 +1,6 @@
 import PropTypes from 'prop-types';
+import { FaDollarSign } from 'react-icons/fa';
+import { BiBookOpen } from 'react-icons/bi';
 const SingleCourse = ({course, handleSelectButton}) => {
     const {image, course_name, course_details, price, credit_hours} = course
     return (
@@ -11,8 +13,8 @@ const SingleCourse = ({course, handleSelectButton}) => {
     <h2 className="text-[18px] ml-1 text-left font-semibold">{course_name}</h2>
     <p className='text-[13px] ml-1 text-left font-normal'>{course_details}</p>
     <div className='flex gap-3 mt-3 mb-3'>
-        <p> <img src="'/public/dollar-sign 1.svg" alt="" /> Price :  {price}</p>
-        <p>Credit : {credit_hours}</p>
+       <FaDollarSign style={{marginLeft: '3px', marginTop: '3px'}} /> <p> Price :  {price}</p>
+      <BiBookOpen style={{marginLeft: '3px', marginTop: '3px'}}/>  <p> Credit : {credit_hours}</p>
     </div>
     <div>
       <button className="btn w-full mt-4 font-semibold text-white bg-[#2F80ED]" onClick={() => handleSelectButton(course)}>Select</button>
